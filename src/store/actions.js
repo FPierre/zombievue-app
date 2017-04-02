43fda62ed1ba2fb7215c86300af2ee9ee7ea62b0
FPierre <pflauder@gmail.com>
2017-04-02 10:55:53 +0200

    Ajout de l'event WS pour récupérer les undeads qui ont bougé

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,5 +6,9 @@ export default {
   socket_undeadCreated: (context, undeads) => {
     console.log('socket_undeadCreated ', undeads)
     context.commit('UNDEADS', undeads)
+  },
+  socket_undeadsMoved: (context, undeads) => {
+    console.log('socket_undeadsMoved ', undeads)
+    context.commit('UNDEADS', undeads)
   }
 }
